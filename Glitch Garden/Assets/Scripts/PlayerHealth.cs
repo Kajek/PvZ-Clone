242066068696428d51e859ae11994849e6364681
Karol <karol.wychowaniak@gmail.com>
2020-09-16 15:10:18 +0200

    first commit

---
+++ Glitch Garden/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float baseHealth = 3f;
+    float playerHealth;
+    [SerializeField] int damage = 1;
+    Text healthText;
+
+    
+    void Start()
+    {
+        playerHealth = baseHealth - PlayerPrefsController.GetDifficulty();
+        healthText = GetComponent<Text>();
+        UpdateDisplay();
+        Debug.Log("difficulty setting currently is " + PlayerPrefsController.GetDifficulty());
+    }
+    private void UpdateDisplay()
+    {
+        healthText.text = playerHealth.ToString();
+    }
+    public void TakeHealth()
+    {        
+        playerHealth -= damage;
+        UpdateDisplay();    
+        
+        if(playerHealth <= 0)
+        {
+            FindObjectOfType<LevelController>().HandleLoseCondition();            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float rotationSpeed = 300f;
+    [SerializeField] float damage = 50f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);        
+        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+
+        if(attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+                
+    }
+
+}
